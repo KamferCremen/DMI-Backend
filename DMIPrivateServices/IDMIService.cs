48b8588e8f48674fcdd116942d0b043d85e33a76
Kasper Andreas Jensen <rtskasper@gmail.com>
2017-11-22 11:45:59 +0100

    Added model class TemperatureData and made start of webservice

---
+++ DMIPrivateServices/IDMIService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DMIPrivateServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IDMIService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "Godteamplate/skrald")]
+        Boolean Navn();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "Godteamplate/skrald")]
+        Boolean Navn2();
+
+    }
+}
---
+++ DMIPrivateServices/Model/TemperatureData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DMIPrivateServices.Model
+{
+    public class TemperatureData
+    {
+        #region Props
+
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private float _temperature;
+        public float Temperature
+        {
+            get { return _temperature; }
+            set { _temperature = value; }
+        }
+
+        private DateTime _captureTime;
+        public DateTime CaptureTime
+        {
+            get { return _captureTime; }
+            set { _captureTime = value; }
+        }
+
+        #endregion
+
+        public TemperatureData(int id, float temperature, DateTime captureTime)
+        {
+            _id = id;
+            _temperature = temperature;
+            _captureTime = captureTime;
+        }
+
+    }
+}
