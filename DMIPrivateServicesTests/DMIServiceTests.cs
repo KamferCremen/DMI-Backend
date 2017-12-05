b0dfbf6b75eeae7f1031d7c125e2301e06f50d23
Kasper Andreas Jensen <rtskasper@gmail.com>
2017-12-05 12:10:39 +0100

    Der er testet 45,10% af koden tilføjet AddTemperature test.

--- DMIPrivateServicesTests/DMIServiceTests.cs
+++ DMIPrivateServicesTests/DMIServiceTests.cs
@@ -15,11 +15,57 @@ namespace DMIPrivateServices.Tests
         [TestMethod()]
         public void AllTemperaturesTest()
         {
+            //Arrange
+
             IDMIService service = new DMIService();
+
+            //Act
 
             List<TemperatureData> temperatures = service.AllTemperatures();
 
+            //Assert
+
             Assert.IsTrue(temperatures.Count > 0);
         }
+
+
+        [TestMethod()]
+        public void AddTemperatureTest()
+        {
+            //Arrange
+
+            IDMIService service = new DMIService();
+
+            //Act
+
+            List<TemperatureData> temperatureList = service.AllTemperatures();
+
+            int count = temperatureList.Count;
+
+            service.AddTemperature(new TemperatureData {Temperature = 40});
+
+            temperatureList = service.AllTemperatures();
+
+            int newCount = temperatureList.Count;
+
+            //Assert
+
+            Assert.AreEqual(count + 1, newCount);
+        }
+
+        [TestMethod()]
+        public void EditTemperatureTest()
+        {
+            //Arange
+
+            IDMIService service = new DMIService();
+
+            //Act
+
+            //TemperatureData 
+
+            //Assert
+        }
+
     }
 }
