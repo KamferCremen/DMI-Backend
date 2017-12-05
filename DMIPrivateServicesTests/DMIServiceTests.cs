95eb64a7fcb98bf6bbd60fa924c448c1d8f85a3e
Kasper Andreas Jensen <rtskasper@gmail.com>
2017-12-05 11:41:40 +0100

    Der er oprettet Unit test solution der skal håndtere vores unit test. Derudover er der lavet en test som tester at listen ikke er lig 0. 28,17% af koden er testet på dette tidspunkt.

---
+++ DMIPrivateServicesTests/DMIServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DMIPrivateServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMIPrivateServices.Model;
+
+namespace DMIPrivateServices.Tests
+{
+    [TestClass()]
+    public class DMIServiceTests
+    {
+        [TestMethod()]
+        public void AllTemperaturesTest()
+        {
+            IDMIService service = new DMIService();
+
+            List<TemperatureData> temperatures = service.AllTemperatures();
+
+            Assert.IsTrue(temperatures.Count > 0);
+        }
+    }
+}
